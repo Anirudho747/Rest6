989b1be1b273425383f586345812401126584da1
aubergine <anirudho747@gmail.com>
2023-04-20 13:21:35 +0530

    Added Json String for complex Body Parameters

--- src/test/java/Basic/Direct.java
+++ src/test/java/Basic/Direct.java
@@ -13,7 +13,9 @@ public class Direct {
     private String URI = "http://fanamp-staging-api-83717343.us-east-1.elb.amazonaws.com/api/v1/";
     private String deviceToken = "fv1_9EtgcUGYgBxyWm4jCi:APA91bGgcPClIiTLBuAHBvO5KePxO0GIzIPNBCXhtaXO3B0xEgkOS-LylRlem933cd4OKRAYMH_Xc8UesND-hKdjW257umpXy3_53E7UCM73w6UnVDbNHb42zCV2xQvg4D3877V2R1S6 ";
     private String userToken = "Bearer "+"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzQ5LCJtb2JpbGVfbnVtYmVyIjoiKzkxIDgwMDAwMDAwNzAiLCJpYXQiOjE2ODE0ODQyMTZ9.woPowT66EMyBbs3BchAqLKC9I-ejEDDj8dkPRHuEaPQTb5-VTJNBvYSRS2Ar_25f9obb5iKirNxqucaAALPJ-A";
-
+    private String eventURI = "http://fanamp-staging-api-83717343.us-east-1.elb.amazonaws.com/admin/events/";
+    private String authorization="Bearer "+"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidHlwZSI6MSwiZW1haWwiOiJhZG1pbi1kYXNoQGZhbmFtcC5jbyIsImV4cCI6MTY4MjQ5ODAwMSwiaWF0IjoxNjc5OTA2MDAxfQ.kTWnlbvSRSjsQtZc3YxGkofKKkjosJUK9w3W13ddrdQXWq7p9GbgkyppaEIvlZmWomJuQc6Ed38-rTWfD2qF_w";
+    
     @Test(enabled=false)
     public void get_UserDetail()
     {
@@ -89,6 +91,7 @@ public class Direct {
 
     }
 
+
     @Test(enabled=true)
     public void put_updateGroupDetails_withParam()
     {
@@ -118,6 +121,50 @@ public class Direct {
 
         Assert.assertEquals(response.prettyPrint().contains("User created successfully."),true,"Text not present");
     }
+    
+    @Test(enabled=false)
+    public void put_CreateUser_withComplexParam()
+    {
+        RequestSpecification httpRequest = RestAssured
+                                           .given()
+                                           .baseUri(eventURI)
+                                           .basePath("1049/gamification/questions/1172/answer/");
+        // Add headers as key value
+        httpRequest.header("Content-Type", "application/json");
+        httpRequest.header("User-Agent", "PostmanRuntime/7.26.8");
+        httpRequest.header("Connection", "keep-alive");
+        httpRequest.header("Accept-Encoding", "gzip, deflate, br");
+        httpRequest.header("Authorization", authorization);
+
+        //Create a JSON String for parameters
+        String jsonString = 
+        		"{\r\n" + 
+        		" \"questionTemplateId\" : 7,\r\n” +
+        		" \"correctAnswer\" : {\r\n”+
+        		" \”answer\” : [ \r\n” +
+        		" { \r\n" +
+        		" \"optionNumber\" :\”2\”,\r\n”+
+        		" \”optionText\” : \”Opt2\”,\r\n” +
+        		" }\r\n”+
+                " ]\r\n” +
+                " }\r\n” +
+        		"}";
+
+        //Add Key-Value sets to the JSON Object
+        requestParams.put("date_of_birth", "1980-10-24");
+        requestParams.put("mobile_number", "+91 8139779453");
+        requestParams.put("referred_by", "7c99hbf5");
+
+        //Add parameters saved in form of Json String
+        httpRequest.body(jsonString());
+
+        Response response = httpRequest.request(Method.POST);
+
+        Assert.assertEquals(response.getStatusCode(),201,"Code Mismatch");
+
+        Assert.assertEquals(response.prettyPrint().contains("User created successfully."),true,"Text not present");
+
+    }
 
     @Test
     public void delete_removeUser()
@@ -140,3 +187,39 @@ public class Direct {
     }
 
 }
+
+
+
+/*
+
+
+http://makeseleniumeasy.com/2019/12/02/rest-assured-tutorial-10-lets-write-first-put-request-in-rest-assured/ 
+
+{
+    "firstname" : "Amod",
+    "lastname" : "Mahajan",
+    "totalprice" : 111,
+    "depositpaid" : true,
+    "bookingdates" : {
+        "checkin" : "2018-01-01",
+        "checkout" : "2019-01-01"
+    },
+    "additionalneeds" : "Breakfast"
+}
+
+
+String jsonString = 
+"{\r\n" + 
+"\"firstname\" : \"Amod\",\r\n" +
+"\"lastname\" : \"Mahajan\",\r\n"+
+"\"totalprice\" : 111,\r\n" +
+"\"depositpaid\" : true,\r\n" +
+"\"bookingdates\" : {\r\n"+
+"\"checkin\" : \"2018-01-01\",\r\n" +
+"\"checkout\" : \"2019-01-01\"\r\n"+
+ "},\r\n" +
+ "\"additionalneeds\" : \"Breakfast\"\r\n" +
+"}";
+
+
+*/
